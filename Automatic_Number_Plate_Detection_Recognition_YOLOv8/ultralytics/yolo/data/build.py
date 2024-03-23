cee5d8419fd725e25cb092e15a337c4480b7d704
Elen Serra <93878271+Elenserra@users.noreply.github.com>
2024-03-23 19:16:21 -0300

    Update build.py

--- Automatic_Number_Plate_Detection_Recognition_YOLOv8/ultralytics/yolo/data/build.py
+++ Automatic_Number_Plate_Detection_Recognition_YOLOv8/ultralytics/yolo/data/build.py
@@ -55,7 +55,7 @@ def seed_worker(worker_id):
 
 
 def build_dataloader(cfg, batch_size, img_path, stride=32, label_path=None, rank=-1, mode="train"):
-    assert mode in ["train", "val"]
+    assert mode in ["train", "val", "test"]
     shuffle = mode == "train"
     if cfg.rect and shuffle:
         LOGGER.warning("WARNING ⚠️ --rect is incompatible with DataLoader shuffle, setting shuffle=False")
